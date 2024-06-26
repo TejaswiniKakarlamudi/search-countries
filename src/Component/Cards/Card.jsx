e3aafab69be75aef04c8740829fff0a733e33df3
TejaswiniKakarlamudi <kakarlamuditejaswini@gmail.com>
2024-06-27 00:00:14 +0530

    Countries app test case

--- src/Component/Cards/Card.jsx
+++ src/Component/Cards/Card.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Card({ data }) {
   return (
     <div className='countryCard'>
-      <div className="card" style={{ width: '80px', height:'auto', margin:'15px'}}>
+      <div className={`card ${data.name.common}`} style={{ width: '80px', height:'auto', margin:'15px'}}>
         <img src={data.flags.png} className="card-img-top countryFlag" alt={data.name.common} style={{ width: '80px', height: '80px', objectFit: 'cover' , boxShadow: '0 2px 4px rgba(0,0,0,0.5)'  }} />
         <div className="card-body">
           <h2 className="card-title name">{data.name.common}</h2>
