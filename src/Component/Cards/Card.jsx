1c8303e163643606f875fd15bbc0a17b9e5794c7
TejaswiniKakarlamudi <kakarlamuditejaswini@gmail.com>
2024-06-26 22:34:07 +0530

    Countries app

--- src/Component/Cards/Card.jsx
+++ src/Component/Cards/Card.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 function Card({ data }) {
   return (
-    <div >
-      <div className="card countryCard" style={{ width: '80px', height:'auto', margin:'15px'}}>
-        <img src={data.flags.png} className="card-img-top" alt={data.name.common} style={{ width: '80px', height: '80px', objectFit: 'cover' , boxShadow: '0 2px 4px rgba(0,0,0,0.5)'  }} />
+    <div className='countryCard'>
+      <div className="card" style={{ width: '80px', height:'auto', margin:'15px'}}>
+        <img src={data.flags.png} className="card-img-top countryFlag" alt={data.name.common} style={{ width: '80px', height: '80px', objectFit: 'cover' , boxShadow: '0 2px 4px rgba(0,0,0,0.5)'  }} />
         <div className="card-body">
-          <h5 className="card-title ">{data.name.common}</h5>
+          <h2 className="card-title name">{data.name.common}</h2>
         </div>
       </div>
     </div>
